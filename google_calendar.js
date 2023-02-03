2dc098cdba3cba0f0e15644b1867fad1cd65bba6
sourabhchauhan25 <70144235+sourabhchauhan25@users.noreply.github.com>
2023-02-04 01:17:17 +0530

    Update google_calendar.js

--- google_calendar.js
+++ google_calendar.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const request = require('request');
 const app = express();
 
 app.get('/rest/v1/calendar/init', function(req, res) {
-  res.send('Google Calendar Init View');
+  const code = req.query.code;
+
+  const options = {
+    method: 'POST',
+    url: 'https://oauth2.googleapis.com/token',
+    form: {
+      code: code,
+      client_id: 'your-client-id',
+      client_secret: 'your-client-secret',
+      redirect_uri: 'your-redirect-uri',
+      grant_type: 'authorization_code'
+    }
+  };
+
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+
+    const accessToken = JSON.parse(body).access_token;
+
+    res.send(`Access Token: ${accessToken}`);
+  });
 });
 
 const port = process.env.PORT || 3000;
